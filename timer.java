be2613021e4ca07dc202101e4addc8ae698dde48
Kartik Vedalaveni <bytecar@gmail.com>
2013-01-08 23:27:46 -0600

    Initial project Import!
    Importing partial work done by me from old repository..

---
+++ timer.java
@@ -0,0 +1,34 @@
+
+
+public class timer {
+
+  public timer() {
+    TimerIsRunning = false;
+    ElapsedMs   = 0;
+  }
+  public void Start() {
+    TimerIsRunning = true;
+    StartReading   = System.currentTimeMillis();
+  }
+  public void Stop() {
+    TimerIsRunning = false;
+    ElapsedMs += System.currentTimeMillis() - StartReading;
+  }
+  public void Reset() {
+    TimerIsRunning = false;
+    ElapsedMs   = 0;
+  }
+  public boolean IsRunning() {
+    return TimerIsRunning;
+  }
+  public double ElapsedTime() {
+    if ( !TimerIsRunning )
+      return (double) ElapsedMs/1000;
+    else
+      return (double) (ElapsedMs + System.currentTimeMillis() - StartReading)/1000;
+  }
+  private boolean TimerIsRunning;
+  private long ElapsedMs;
+  private long StartReading;
+
+}
